e1d0118dc31fa4fe9ddb6ae2f51ae6db96b8bade
Arjun Sen <arjunsen1997@gmail.com>
2021-06-19 13:30:58 +0530

    My third commit

---
+++ untitled.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    char name[] = "John";
+    string name1 = "John";
+    cout << "C++ is nice" << " test" << endl;
+    cin >> name1;
+    getline(cin, name1);
+    cout << name; 
+    int var;
+    int var;
+    return 0;
+}
