01cf97007e8e3aed13a1a263405812ac386b773d
Arjun Sen <arjunsen1997@gmail.com>
2021-06-27 03:43:48 +0530

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+            
+        }
+
+        public void SetUpGame()
+        {
+            var animalEmoji = new List<string>
+            {
+                "💖", "💖",
+                "💖", "💖",
+                "💖", "💖",
+                "💖", "💖",
+                "💖", "💖",
+                "💖", "💖",
+                "💖", "💖",
+                "💖", "💖"
+            };
+
+            var random = new Random();
+
+            foreach (var textBlock in MainGrid.Children.OfType<TextBlock>())
+            {
+                var randomIndex = random.Next(animalEmoji.Count);
+                textBlock.Text = animalEmoji[randomIndex];
+                animalEmoji.RemoveAt(randomIndex);
+            }
+        }
+    }
+}
